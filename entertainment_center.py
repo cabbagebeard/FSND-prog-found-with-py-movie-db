a456c5641d96c3fd6ccd50b6b1be31b8d22cccfc
kevin <kwindisch1992@gmail.com>
2016-07-21 18:40:39 -0500

    Make sure fresh_tomatoes works

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,4 +1,5 @@
 import media
+import fresh_tomatoes
 
 toy_story = media.Movie("Toy Story",
                         "A story of a boy and his toys that come to life.",
@@ -10,4 +11,11 @@ avatar = media.Movie("Avatar",
                      "http://i46.tinypic.com/1zly8if.jpg",
                      "https://www.youtube.com/watch?v=5PSNL1qE6VY")
 
-avatar.show_trailer()
+space_odyssey = media.Movie("2001: A Space Odyssey",
+                            "An imposing black structure provides a connection between the past and the future",
+                            "http://t0.gstatic.com/images?q=tbn:ANd9GcQdmmLZ7lXsw1WRy7c5qN3mka2e9ANSpHIG2INi53P6OVS8KyJo",
+                            "https://www.youtube.com/watch?v=lfF0vxKZRhc")
+
+movies = [toy_story, avatar, space_odyssey]
+
+fresh_tomatoes.open_movies_page(movies)
