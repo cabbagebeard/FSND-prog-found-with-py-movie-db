c528d4c79500bffe45556233d5b98897be5cc360
kevin <kwindisch1992@gmail.com>
2016-07-21 18:08:36 -0500

    Add show_trailer function

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,3 +1,13 @@
 import media
 
-toy_story = media.Movie()
+toy_story = media.Movie("Toy Story",
+                        "A story of a boy and his toys that come to life.",
+                        "http://vignette4.wikia.nocookie.net/disney/images/4/4c/Toy-story-movie-posters-4.jpg/revision/latest?cb=20140816182710",
+                        "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+
+avatar = media.Movie("Avatar",
+                     "A marine on an alien planet",
+                     "http://i46.tinypic.com/1zly8if.jpg",
+                     "https://www.youtube.com/watch?v=5PSNL1qE6VY")
+
+avatar.show_trailer()
--- media.py
+++ media.py
@@ -1,3 +1,5 @@
+import webbrowser
+
 class Movie():
 
     def __init__(self, title, storyline, image, trailer):
@@ -5,4 +7,8 @@ class Movie():
         self.storyline = storyline
         self.poster_image_url = image
         self.trailer_youtube_url = trailer
+
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
+        
         
