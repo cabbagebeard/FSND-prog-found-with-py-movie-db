e51010578df480373953926839cb02d6274b5ad9
kevin <kwindisch1992@gmail.com>
2016-07-21 17:42:31 -0500

    Init commit

---
+++ media.py
@@ -0,0 +1,8 @@
+class Movie():
+
+    def __init__(self, title, storyline, image, trailer):
+        self.title = title
+        self.storyline = storyline
+        self.poster_image_url = image
+        self.trailer_youtube_url = trailer
+        
